74b98b70e77059e019f465004abc792208ae1524
Tim <141640156+TStokes06@users.noreply.github.com>
2024-01-12 18:03:31 +0000

    TS06

--- src/models.py
+++ src/models.py
@@ -7,27 +7,75 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
+class User(Base):
+    __tablename__ = 'user'
     # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+    userName = Column(String(250), nullable=False)
+    firstName = Column(String(250),nullable=False)
+    lastName = Column(String(250),nullable=False)
+    Email = Column(String(250),nullable=False)
 
-class Address(Base):
-    __tablename__ = 'address'
+    def serialize (self):
+        return{
+            "userName":self.userName,
+            "firstName":self.userName,
+            "lastName":self.lastName,
+            "Email":self.Email,
+        }
+
+class Follower(Base):
+    __tablename__ = 'follower'
     # Here we define columns for the table address.
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    followingUserID = Column(Integer,ForeignKey("user.id"))
+    followedUserID = Column(Integer,ForeignKey("user.id"))
 
-    def to_dict(self):
-        return {}
+class Post(Base):
+    __tablename__ = 'post'
+    # Here we define columns for the table address.
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    userID=relationship(User)
+    caption = Column(String)
+    postDate = Column(Integer)
+    postTime = Column(Integer)
 
+class Media(Base):
+    __tablename__ = 'media'
+    # Here we define columns for the table address.
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    postID = Column(Integer,ForeignKey("post.id"))
+    type = Column(String)
+    url = Column(String)
+
+    def serialize(self):
+        return {
+            "type":self.type,
+            "url":self.url,
+        }
+
+class Comment(Base):
+    __tablename__ = 'comment'
+    # Here we define columns for the table address.
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    commentText = Column(String,nullable=True)
+    authorID = Column(Integer,ForeignKey("user.id"))
+    postID = Column(Integer,ForeignKey("post.id"))
+
+    def serialize(self):
+        return {
+            "commentText":self.commentText,
+            "authorID":self.authorID,
+            "postID":self.postID,
+        }
+
+
+ 
 ## Draw from SQLAlchemy base
 try:
     result = render_er(Base, 'diagram.png')
